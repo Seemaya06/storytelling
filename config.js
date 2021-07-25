79a6d83b9b146234d45037dce4536838eeb0748d
Seemaya06 <71091580+Seemaya06@users.noreply.github.com>
2021-07-25 11:38:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBnWP4Unah-CS_Rwj6QKsNwy4hFTo50QwM",
+  authDomain: "story-4a4c6.firebaseapp.com",
+  projectId: "story-4a4c6",
+  storageBucket: "story-4a4c6.appspot.com",
+  messagingSenderId: "948840703303",
+  appId: "1:948840703303:web:03cd38bcb7d65b4aec9862",
+  measurementId: "G-TR1LKVNTJK"
+};
